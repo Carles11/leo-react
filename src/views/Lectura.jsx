05e4b95f2d44eaa2dff1406529abbf77ff4a06e8
Carles <carles@crix.design>
2020-12-06 19:23:24 +0100

    reversed years order for texts

--- src/views/Lectura.jsx
+++ src/views/Lectura.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from 'react';
+import Helmet from 'react-helmet';
 
-import withScroll from '../components/HOC/withScroll'
-import * as API from '../utils/API'
+import withScroll from '../components/HOC/withScroll';
+import * as API from '../utils/API';
 
 class Lectura extends React.PureComponent {
-  state = { data: [], audio: '' }
+  state = { data: [], audio: '' };
 
   async componentDidMount() {
-    const promise = await API.get('documents')
+    const promise = await API.get('documents');
 
     if (promise.success) {
-      this.setState({ data: promise.data })
+      this.setState({ data: promise.data });
     }
   }
 
-  handleAudio = audio => {
-    this.setState({ audio })
-  }
+  handleAudio = (audio) => {
+    this.setState({ audio });
+  };
 
   render() {
-    const { DIC } = this.props
-    const { data, audio } = this.state
+    const { DIC } = this.props;
+    const { data, audio } = this.state;
 
     return (
-      <section className='app-content pb2rem mb2rem'>
+      <section className="app-content pb2rem mb2rem">
         <Helmet
           title={DIC.NAV_TEXTOS}
           meta={[
@@ -33,71 +33,79 @@ class Lectura extends React.PureComponent {
           ]}
         />
         <header>
-          <h1 className='tit-header mb2rem'>{DIC.NAV_TEXTOS}</h1>
+          <h1 className="tit-header mb2rem">{DIC.NAV_TEXTOS}</h1>
         </header>
-        <div className='app-section-width app-section-boxes'>
+        <div className="app-section-width app-section-boxes">
           {!!data.length &&
             data
-              .sort((a, b) => (a.year > b.year ? -1 : 1))
-              .map(d => (
+              .sort((a, b) => (a.year > b.year ? 1 : -1))
+              .map((d) => (
                 <React.Fragment key={d._id}>
-                  {d.year === 2021 ?
-                  <h2 className='subtit-section subtit-section-underline txt-center w100'>
-                    {d.title}
-                    <br />
-                   <small className='txt-center'>
-                      * Textos A1-B1 leídos por Sara Casado.                
-                    </small>< br/>
-                    <small className='txt-center margin-text'>
-                      * Textos B2 leídos por:       
-                    </small>
-               
-                    <ul className="decoration-none">
-                    <small >
-                      <li>Texto 1: Norma Palacios</li>
+                  {d.year === 2021 ? (
+                    <h2 className="subtit-section subtit-section-underline txt-center w100">
+                      {d.title}
+                      <br />
+                      <small className="txt-center">
+                        * Textos A1-B1 leídos por Sara Casado.
                       </small>
-                    <small >
-                      <li>Texto 2: Gema Bonnín</li>
+                      <br />
+                      <small className="txt-center margin-text">
+                        * Textos B2 leídos por:
                       </small>
-                    <small >
-                      <li>Texto 3: Carolín Fuentes</li>
+                      <ul className="decoration-none">
+                        <small>
+                          <li>Texto 1: Norma Palacios</li>
+                        </small>
+                        <small>
+                          <li>Texto 2: Gema Bonnín</li>
+                        </small>
+                        <small>
+                          <li>Texto 3: Carolín Fuentes</li>
+                        </small>
+                        <small>
+                          <li>Texto 4: Heidy González</li>
+                        </small>
+                        <small>
+                          <li>Texto 5: María E. Martínez</li>
+                        </small>
+                      </ul>{' '}
+                    </h2>
+                  ) : d.year !== 2018 ? (
+                    <h2 className="subtit-section subtit-section-underline txt-center w100">
+                      {d.title}
+                      <br />
+                      <small className="txt-center">
+                        * Textos leídos por Sara Casado Ocaña.
                       </small>
-                    <small >
-                      <li>Texto 4: Heidy González</li>
-                      </small>
-                    <small >
-                      <li>Texto 5: María E. Martínez</li>
-                      </small>
-                      </ul> </h2> : d.year !== 2018  ? <h2 className='subtit-section subtit-section-underline txt-center w100'>
-                    {d.title}
-                    <br />
-                   <small className='txt-center'>
-                      * Textos leídos por Sara Casado Ocaña.                
-                    </small>< br/>
-                   </h2> : <h2 className='subtit-section subtit-section-underline txt-center w100'>
-                    {d.title}
-                    <br />
-                 
-                   </h2> }
+                      <br />
+                    </h2>
+                  ) : (
+                    <h2 className="subtit-section subtit-section-underline txt-center w100">
+                      {d.title}
+                      <br />
+                    </h2>
+                  )}
 
-                  {d.projects.map(project => (
+                  {d.projects.map((project) => (
                     <article
                       key={project.title}
-                      className='app-section-box mb2rem'>
-                      <h2 className='txt-highlight'>{project.title}</h2>
-                      <ul className='app-list'>
-                        {project.items.map(item => (
-                          <li key={item.title} className='app-list-item'>
-                            <header className='app-list-header'>
+                      className="app-section-box mb2rem"
+                    >
+                      <h2 className="txt-highlight">{project.title}</h2>
+                      <ul className="app-list">
+                        {project.items.map((item) => (
+                          <li key={item.title} className="app-list-item">
+                            <header className="app-list-header">
                               <h2>{item.title}</h2>
-                              <div className='app-list-content-btn'>
+                              <div className="app-list-content-btn">
                                 {item.audio && (
                                   <button
                                     aria-label={`Escuchar el audio '${item.title}'`}
                                     onClick={() => this.handleAudio(item.audio)}
-                                    className='app-list-btn icon-headphones'
-                                    title={`Escuchar el audio '${item.title}'`}>
-                                    <span className='hidden'>
+                                    className="app-list-btn icon-headphones"
+                                    title={`Escuchar el audio '${item.title}'`}
+                                  >
+                                    <span className="hidden">
                                       {`Escuchar el audio '${item.title}'`}
                                     </span>
                                   </button>
@@ -107,9 +115,10 @@ class Lectura extends React.PureComponent {
                                   aria-label={`Descargar el texto '${item.title}'`}
                                   download={item.url}
                                   href={item.url}
-                                  className='app-list-btn icon-arrow-down-circle'
-                                  title={`Descargar el texto '${item.title}'`}>
-                                  <span className='hidden'>
+                                  className="app-list-btn icon-arrow-down-circle"
+                                  title={`Descargar el texto '${item.title}'`}
+                                >
+                                  <span className="hidden">
                                     {`Descargar el texto '${item.title}'`}
                                   </span>
                                 </a>
@@ -117,10 +126,11 @@ class Lectura extends React.PureComponent {
                             </header>
                             {audio === item.audio && (
                               <audio
-                                className='app-audio'
+                                className="app-audio"
                                 src={audio}
-                                controls='controls'
-                                autoPlay>
+                                controls="controls"
+                                autoPlay
+                              >
                                 Your browser does not support the{' '}
                                 <code>audio</code> element.
                               </audio>
@@ -134,11 +144,11 @@ class Lectura extends React.PureComponent {
               ))}
         </div>
       </section>
-    )
+    );
   }
 }
 
-const LecturaWithScroll = withScroll(Lectura)
+const LecturaWithScroll = withScroll(Lectura);
 
-export const Unwrapped = Lectura
-export default LecturaWithScroll
+export const Unwrapped = Lectura;
+export default LecturaWithScroll;
