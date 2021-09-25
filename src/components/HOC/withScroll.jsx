d3bf90381702bc405294f605979af356e562f6ae
Carles <carles@crix.design>
2021-09-25 20:32:23 +0200

    fix

--- src/components/HOC/withScroll.jsx
+++ src/components/HOC/withScroll.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const withScroll = Component => class extends React.Component {
-  componentDidMount() {
-    const section = document.querySelector('.app-content')
-
-    if (section) {
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      })
+const withScroll = (Component) =>
+  class extends React.Component {
+    componentDidMount() {
+      const section = document.querySelector('.app-content');
+      if (section && section !== null) {
+        section.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
     }
-  }
 
-  render() {
-    return <Component {...this.props} {...this.state} />
-  }
-}
+    render() {
+      return <Component {...this.props} {...this.state} />;
+    }
+  };
 
-export default withScroll
+export default withScroll;
