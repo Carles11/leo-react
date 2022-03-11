f5f08122988527656520315e8567c2e3076c2486
Carles <carles@crix.design>
2022-03-11 12:24:36 -0300

    set photos default to 2019

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -25,7 +25,7 @@ class ImageGallery extends React.Component {
     const currentYear = newDate.getFullYear();
     if (position !== prevState.position && !lazyLoad) {
       this.setState((oldState) => ({ lazyLoad: !oldState.lazyLoad }));
-      this.getPhotos(currentYear - 2); // we show 2019 by defect
+      this.getPhotos(currentYear - 3); // we show 2019 by dafault
     }
   }
 
