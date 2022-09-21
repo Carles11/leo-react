a98ce8605fc4156613ef93591fb8fb43a7165fac
Carles <carles@crix.design>
2022-09-21 15:22:06 -0300

    admin mod

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,59 +1,59 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Button from './Button'
+import Button from './Button';
 
-const Item = props => {
-  const { item, handleRemove, handleShow } = props
+const Item = (props) => {
+  const { item, handleRemove, handleShow } = props;
 
   return (
     <React.Fragment>
-      <li className='app-list-item'>
-        <header className='app-list-header'>
+      <li className="app-list-item">
+        <header className="app-list-header">
           <h2>{item.name}</h2>
-          <div className='app-list-content-btn'>
+          <div className="app-list-content-btn">
             {/* <button
               className="app-list-btn icon-edit-2"
               arial-label="Edit"
             >
             </button> */}
             <button
-              className='app-list-btn icon-x-circle'
-              arial-label='Remove Item'
+              className="app-list-btn icon-x-circle"
+              arial-label="Remove Item"
               data-id={item._id}
               onClick={handleRemove}
             />
             <button
-              className='app-list-btn icon-arrow-down-circle'
-              arial-label='Show Item'
+              className="app-list-btn icon-arrow-down-circle"
+              arial-label="Show Item"
               data-id={item._id}
               onClick={handleShow}
             />
           </div>
         </header>
-        <section data-id={item._id} className='app-list-content show'>
+        <section data-id={item._id} className="app-list-content show">
           <article>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>Teléfono:</small>
               {item.phone}
             </p>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>Dirección:</small>
               {item.address}
             </p>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>Localidad:</small>
               {item.city}
             </p>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>CP:</small>
               {item.cp}
             </p>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>Contacto:</small>
               {item.contact}
             </p>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>Email:</small>
               <Button
                 label={item.email}
@@ -61,21 +61,25 @@ const Item = props => {
                 external
               />
             </p>
-            <p className='app-list-content-item'>
+            <p className="app-list-content-item">
               <small>Categorias:</small>
               {item.category.join(', ')}
+            </p>
+            <p className="app-list-content-item">
+              <small>Edición:</small>
+              {item.year}
             </p>
           </article>
         </section>
       </li>
     </React.Fragment>
-  )
-}
+  );
+};
 
 Item.propTypes = {
   item: PropTypes.object.isRequired,
   handleRemove: PropTypes.func.isRequired,
   handleShow: PropTypes.func.isRequired,
-}
+};
 
-export default Item
+export default Item;
--- src/components/PrintComponent.jsx
+++ src/components/PrintComponent.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Item from './Item'
+import Item from './Item';
 
 class PrintComponent extends React.Component {
-	static propTypes = {
-		data: PropTypes.array.isRequired,
-		handleRemove: PropTypes.func.isRequired,
-		handleShow: PropTypes.func.isRequired,
-	}
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    handleRemove: PropTypes.func.isRequired,
+    handleShow: PropTypes.func.isRequired,
+  };
 
-	render() {
-		const { data, handleRemove, handleShow } = this.props
+  render() {
+    const { data, handleRemove, handleShow } = this.props;
 
-		return (
-			<ul className="app-list">
-				{!!data.length &&
-					data.map(item => (
-						<Item key={item._id} item={item} handleRemove={handleRemove} handleShow={handleShow} />
-					))}
-				{data.length === 0 && <p className="txt">Todavía no hay ningún item en el listado.</p>}
-			</ul>
-		)
-	}
+    return (
+      <ul className="app-list">
+        {!!data.length &&
+          data.map((item) => (
+            <Item
+              key={item._id}
+              item={item}
+              handleRemove={handleRemove}
+              handleShow={handleShow}
+            />
+          ))}
+        {data.length === 0 && (
+          <p className="txt">Todavía no hay ningún colegio en el listado.</p>
+        )}
+      </ul>
+    );
+  }
 }
 
-export default PrintComponent
+export default PrintComponent;
