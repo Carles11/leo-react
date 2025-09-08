83385d3710395d9dcc6668114fde569a5371538f
Carles <c.delriofrances@gmail.com>
2025-09-08 15:18:30 -0300

    updated criterios

--- src/views/Criterios.jsx
+++ src/views/Criterios.jsx
@@ -24,32 +24,37 @@ const Criterios = (props) => {
           <strong className="txt-highlight">
             Criterios de lectura para el jurado:&nbsp;
           </strong>
-          La "calidad" de la lectura se determinará según los siguientes dos
+          la “calidad” de la lectura se determinará según los siguientes
           criterios:
         </p>
         <ul>
-          <li>Pronunciación / entonación</li>
-          <li>Comprensión / interpretación</li>
+          {' '}
+          <strong className="txt-highlight">
+            <li>Pronunciación / entonación</li>
+            <li>Comprensión / interpretación</li>{' '}
+          </strong>
         </ul>
         <p className="txt-left">
-          La puntuación será de 1 a 5, para cada una de las tres categorías,
-          siendo 5 el máximo.
+          La puntuación será de 1 a 5 para cada una de las tres categorías,
+          siendo 5 la puntuación máxima.
         </p>
         <p className="txt-left">
           <strong className="txt-highlight">
-            Pronunciación/entonación:&nbsp;
+            Pronunciación y entonación:&nbsp;
           </strong>
-          pronunciación clara y correcta (en los niveles A1 y A2 no se tendrán
-          en cuenta deficiencias en la pronunciación de la "r/rr"), acentuación
-          correcta y velocidad de lectura. No se penalizará la autocorrección.
+          Se valorará la pronunciación clara y correcta (en los niveles A1 y A2
+          no se tendrán en cuenta las deficiencias en la pronunciación de la
+          “r/rr”), la acentuación correcta y la velocidad de lectura. No se
+          penalizará la autocorrección.
         </p>
         <p className="txt-left">
           <strong className="txt-highlight">
-            Comprensión/interpretación:&nbsp;
+            Comprensión e interpretación:&nbsp;
           </strong>
-          el candidato demuestra la comprensión de lo leído a través de la
-          segmentación correcta de frases y el uso de recursos prosódicos. Así
-          mismo consigue reflejar el tono y el contexto ambiental de la lectura.
+          El candidato demuestra la comprensión de lo leído mediante una
+          segmentación correcta de las frases y el uso adecuado de los recursos
+          prosódicos. Así mismo, consigue reflejar el tono y el contexto
+          ambiental de la lectura.
         </p>
       </article>
     </div>
